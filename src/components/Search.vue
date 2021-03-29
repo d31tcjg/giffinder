e6382f32cc392f86c0618aace3188421bcbfbb0e
Cesar Garcia <d31tcjg@gmail.com>
2021-03-28 22:37:33 -0500

    Gif App

---
+++ src/components/Search.vue
@@ -0,0 +1,13 @@
+<template>
+  <input
+    class="container block mx-auto rounded-full mt-8 text-2xl text-center p-2 outline-none focus:ring-4 focus:ring-green-500 focus:ring-opactiy-50"
+    type="text"
+    placeholder="I'm looking for..."
+    v-model="searchItem"
+    @change="getGifs"
+  />
+</template>
+
+<script setup>
+import { searchItem, getGifs } from "../helpers/useGifs";
+</script>
